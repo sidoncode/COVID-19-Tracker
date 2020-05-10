01194a9b5a51a49358bca36f469fdb39158d7965
Siddharth Sharma <40432616+sidoncode@users.noreply.github.com>
2020-05-10 12:01:16 +0530

    Initial commit

---
+++ app/src/main/java/com/example/covid_19_tracker/ui/dashboard/DashboardViewModel.java
@@ -0,0 +1,19 @@
+package com.example.covid_19_tracker.ui.dashboard;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class DashboardViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public DashboardViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is dashboard fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
